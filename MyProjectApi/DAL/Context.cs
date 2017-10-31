62fe37717500fa772c5e5fabbe58d01e5ce1cadd
Hari Balakrishnan <good2furious@gmail.com>
2017-10-31 17:22:11 +0000

    Unit of Work Instantiation is required

--- MyProjectApi/DAL/Context.cs
+++ MyProjectApi/DAL/Context.cs
@@ -5,7 +5,7 @@ using MyProjectApi.Models;
 
 namespace MyProjectApi.DAL
 {
-    public class Context : DbContext, IUnitOfWork, IDisposable
+    public class Context : DbContext, IUnitOfWork
     {
         public Context() : base("DatabaseContext")
         {
@@ -26,28 +26,6 @@ namespace MyProjectApi.DAL
         {
             return base.Set<T>();
         }
-
-        #region Dispose being used for context being called
-        private bool _disposed;
-
-        protected override void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    base.Dispose();
-                }
-            }
-            _disposed = true;
-        }
-
-        public new void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
---
+++ MyProjectApi/DAL/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MyProjectApi.DAL
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly Context _context;
+
+        public UnitOfWork(Context context)
+        {
+            _context = context;
+        }
+        
+        public void Save()
+        {
+            _context.Save();
+        }
+
+        public IDbSet<T> Set<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+        
+        #region Dispose being used for context being called
+        private bool _disposed;
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
